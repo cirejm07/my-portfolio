2c7b7efd9d120cecdfe66c2bc67e2f45e5392da3
cirejm07 <moreno.jeric1995@gmail.com>
2021-11-03 21:01:11 +0800

    includes online portfolio

--- public/index.js
+++ public/index.js
@@ -1,52 +1,29 @@
-
- 
-
-let fName = document.getElementById("fName")
 let email = document.getElementById("txt-email")
-let message = document.getElementById("txt-textarea")
-let contactForm = document.getElementById('contact-form')
-
-
-contactForm.addEventListener('submit',(e) => {
-  e.preventDefault()
-
-  let formData = {
-    fName: fName.value,
-    email: email.value,
-    message: message.value
-  }
-
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST','/');
-  xhr.setRequestHeader('content-type', 'application/json')
-  xhr.onload = function(){
-    console.log(xhr.responseText);
-    if(xhr.responseText == 'success'){
-      Swal.fire({
-        title: '<strong>HTML <u>example</u></strong>',
-        icon: 'info',
-        html:
-          'You can use <b>bold text</b>, ' +
-          '<a href="//sweetalert2.github.io">links</a> ' +
-          'and other HTML tags',
-        showCloseButton: true,
-        showCancelButton: true,
-        focusConfirm: false,
-        confirmButtonText:
-          '<i class="fa fa-thumbs-up"></i> Great!',
-        confirmButtonAriaLabel: 'Thumbs up, great!',
-        cancelButtonText:
-          '<i class="fa fa-thumbs-down"></i>',
-        cancelButtonAriaLabel: 'Thumbs down'
-      })
-      fName.value = '',
-      email.value = '',
-      message.value = ''
-    } else{
-      alert("Something went wrong")
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
     }
-  }
+  }).then(response => {
+    status.innerHTML = Swal.fire({
+      position: 'top-end',
+      icon: `success`,
+      title: `Thank you ${email.value}, Your Message has been sent`,
+      showConfirmButton: false,
+      timer: 2500
+    });
+    form.reset()
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
 
-  xhr.send(JSON.stringify(formData))
 
-})
